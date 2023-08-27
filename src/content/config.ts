25d33909600c3960504463c170c29184f6ea92ad
masayan1126 <masa199311266@gmail.com>
2023-08-27 09:25:21 +0900

    Add collection and var

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+// 2. Define a `type` and `schema` for each collection
+const blogCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.object({
+      url: z.string().url(),
+      alt: z.string(),
+    }),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  blog: blogCollection,
+};
